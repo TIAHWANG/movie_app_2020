b7ba4267bc5a7c2c93bbef0efa48f525a7f5810a
Tia Hwang <tiajshwang@gmail.com>
2020-04-02 20:38:47 +0900

    JSX + props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,55 @@
 import React from "react";
+import PropTypes from "prop-types";
+
+function Dog({ name, picture, cuteness }) {
+    return (
+        <div>
+            <h2>I like {name}</h2>
+            <h4>{cuteness}/5.0</h4>
+            <img src={picture} alt={name} />
+        </div>
+    );
+}
+
+Dog.propTypes = {
+    name: PropTypes.string.isRequired,
+    picture: PropTypes.string.isRequired,
+    cuteness: PropTypes.number.isRequired
+};
+
+const dogILike = [
+    {
+        id: 1,
+        name: "puppies",
+        image: "http://i.ytimg.com/vi/0CwAyJ8M_BA/maxresdefault.jpg",
+        cuteness: 5
+    },
+    {
+        id: 2,
+        name: "cutie",
+        image:
+            "https://barkingroyalty.com/wp-content/uploads/2015/12/Dalmatians-puppy.jpg",
+        cuteness: 4.9
+    },
+    {
+        id: 3,
+        name: "ohmygod",
+        image: "https://i.ytimg.com/vi/Wbcy-luvFJM/maxresdefault.jpg",
+        cuteness: 4.8
+    }
+];
 
 function App() {
     return (
         <div>
-            <h2>AWESOME</h2>
+            {dogILike.map(dog => (
+                <Dog
+                    key={dog.id}
+                    name={dog.name}
+                    picture={dog.image}
+                    cuteness={dog.cuteness}
+                />
+            ))}
         </div>
     );
 }
