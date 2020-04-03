12d12b18c89334f71ff69024171ee9e97c7e0008
Tia Hwang <tiajshwang@gmail.com>
2020-04-03 12:40:48 +0900

    State

--- src/App.js
+++ src/App.js
@@ -1,57 +1,30 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-function Dog({ name, picture, cuteness }) {
-    return (
-        <div>
-            <h2>I like {name}</h2>
-            <h4>{cuteness}/5.0</h4>
-            <img src={picture} alt={name} />
-        </div>
-    );
-}
+class App extends React.Component {
+    state = {
+        count: 0
+    };
 
-Dog.propTypes = {
-    name: PropTypes.string.isRequired,
-    picture: PropTypes.string.isRequired,
-    cuteness: PropTypes.number.isRequired
-};
-
-const dogILike = [
-    {
-        id: 1,
-        name: "puppies",
-        image: "http://i.ytimg.com/vi/0CwAyJ8M_BA/maxresdefault.jpg",
-        cuteness: 5
-    },
-    {
-        id: 2,
-        name: "cutie",
-        image:
-            "https://barkingroyalty.com/wp-content/uploads/2015/12/Dalmatians-puppy.jpg",
-        cuteness: 4.9
-    },
-    {
-        id: 3,
-        name: "ohmygod",
-        image: "https://i.ytimg.com/vi/Wbcy-luvFJM/maxresdefault.jpg",
-        cuteness: 4.8
+    add = () => {
+        this.setState(current => ({ count: current.count + 1 }));
+    };
+    minus = () => {
+        this.setState(current => ({ count: current.count - 1 }));
+    };
+    render() {
+        console.log("I am rendering");
+        return (
+            <div>
+                <h1>The number is {this.state.count}</h1>
+                {/* this.add(): immediately call function
+                this.add: only when click button, call function */}
+                <button onClick={this.add}>Add</button>
+                <button onClick={this.minus}>Minus</button>
+                {/* click button => setState 호출 => refresh state => render function 호출 with new state */}
+            </div>
+        );
     }
-];
-
-function App() {
-    return (
-        <div>
-            {dogILike.map(dog => (
-                <Dog
-                    key={dog.id}
-                    name={dog.name}
-                    picture={dog.image}
-                    cuteness={dog.cuteness}
-                />
-            ))}
-        </div>
-    );
 }
 
 export default App;
